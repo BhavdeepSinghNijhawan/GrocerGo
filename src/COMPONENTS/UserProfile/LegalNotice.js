0689a9f0e1f64594df8921e52e24124bcb579626
Bhavdeep Singh Nijhawan <143419096+BhavdeepSinghNijhawan@users.noreply.github.com>
2024-05-21 23:04:04 +0530

    Update LegalNotice.js

--- src/COMPONENTS/UserProfile/LegalNotice.js
+++ src/COMPONENTS/UserProfile/LegalNotice.js
@@ -1,6 +1,7 @@
+// Author: Bhavdeep Singh Nijhawan
+
 import React from 'react'
 import './LegalNotice.css'
-
 const LegalNotice = () => {
     return (
         <div className='legalnotice'>
@@ -12,21 +13,18 @@ const LegalNotice = () => {
                     Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
                 </p>
             </div>
-
             <div className='legalnoticein'>
                 <h2>2. Terms of Use</h2>
                 <p>
                     Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
                 </p>
             </div>
-
             <div className='legalnoticein'>
                 <h2>3. Terms of Use</h2>
                 <p>
                     Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
                 </p>
             </div>
-
             <div className='legalnoticein'>
                 <h2>4. Terms of Use</h2>
                 <p>
@@ -36,5 +34,4 @@ const LegalNotice = () => {
         </div>
     )
 }
-
 export default LegalNotice
